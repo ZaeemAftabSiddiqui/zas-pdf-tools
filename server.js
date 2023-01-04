1fd736cb9846c7e5c62eee06120a96b8bbe753cf
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2023-01-04 14:27:33 +0500

    add multer and use it

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const path = require("path");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "templates/index.html"));
+});
+app.post("/merge", upload.array("pdfs", 2), function (req, res, next) {
+  console.log(req.files);
+  res.send({ data: req.files });
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
 });
 
 app.listen(port, () => {
