ed75b6987f7799e0dff081ffc5ab44557f20d764
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2023-01-04 16:24:25 +0500

    used node package(pdf merger) for merge pdf

--- merge.js
+++ merge.js
@@ -7,8 +7,9 @@ const mergePdfs = async (p1, p2) => {
   await merger.add(p2);
   let d = new Date().getTime();
   await merger.save(`public/${d}.pdf`); //save under given name and reset the internal document
-
+  return d;
   // Export the merged PDF as a nodejs Buffer
   // const mergedPdfBuffer = await merger.saveAsBuffer();
   // fs.writeSync('merged.pdf', mergedPdfBuffer);
 };
+module.exports = { mergePdfs };
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const path = require("path");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { mergePdfs } = require("./merge");
 const app = express();
+
+const upload = multer({ dest: "uploads/" });
+app.use("/static", express.static("public"));
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "templates/index.html"));
 });
-app.post("/merge", upload.array("pdfs", 2), function (req, res, next) {
+app.post("/merge", upload.array("pdfs", 2), async (req, res, next) => {
   console.log(req.files);
-  res.send({ data: req.files });
+  let d = await mergePdfs(
+    path.join(__dirname, req.files[0].path),
+    path.join(__dirname, req.files[1].path)
+  );
+  res.redirect(`http://localhost:3000/static/${d}.pdf`);
+  // res.send({ data: req.files });
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
 });
